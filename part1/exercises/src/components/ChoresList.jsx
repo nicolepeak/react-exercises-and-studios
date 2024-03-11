d4e6038cc5f412a46831039b9d4eaa8db2e444e8
nicolepeak <colecpeak@gmail.com>
2024-03-11 14:31:19 -0500

    2nd save

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,13 @@
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div>
+         <h2 className="choresHeading">Things to Do</h2>
+         <ol>
+            <li>Laundry</li>
+            <li>Dishes</li>
+            <li>Clean Bedroom</li>
+            <li>Clean Bathroom</li>
+         </ol>
+      </div>
+   )
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,10 @@
+export default function HobbyLinks() {
+    const hobbyLinks = ["https://www.ea.com/games/the-sims", "https://dnd.wizards.com/"]
+
+    return (
+        <div>
+        <a href="{hobbyLinks[0]}">"The Sims"</a>
+        <a href="{hobbyLinks[1]}">"Dungeons and Dragons"</a>
+        </div>
+    )
+}
